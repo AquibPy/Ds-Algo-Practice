0960a8ae3b97901ab37364e1292e31feb78bdecc
AquibPy <mohdaquib31@gmail.com>
2020-11-20 23:51:51 +0530

    Duplicate element in array

---
+++ Week 2/duplicate_in_array.cpp
@@ -0,0 +1,38 @@
+/*
+Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+There is only one duplicate number in nums, return this duplicate number.
+*/
+
+#include<iostream>
+using namespace std;
+void show_array(int arr[],int size)
+{
+	for(int i=0;i<size;i++)
+	{
+		cout<<arr[i]<<" ";
+	}
+}
+int main()
+{
+	int n,arr[n];
+	cout<<"Enter the Size of an Array: ";
+	cin>>n;
+	cout<<"Enter An Element of an Array: "<<endl;
+	for(int i=0;i<n;i++)
+	{
+		cin>>arr[i];
+	}
+	cout<<"Array is ";
+	show_array(arr,n);
+	
+	for(int i =0;i<n;i++)
+	{
+		for(int j=i+1;j<n;j++)
+		{
+			if(arr[i]==arr[j])
+			{
+				cout<<"\nDuplicate Element is "<<arr[i];
+			}
+		}
+	}
+}
